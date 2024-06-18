68a45dafba7f091e1b927168b3792dee086ad1de
Geethu-007 <geethikede@gmail.com>
2024-06-18 13:28:47 +0530

    Added My 1st Web Site Files

---
+++ Web_Page/cart.js
@@ -0,0 +1,29 @@
+function cart1()
+{
+    document.getElementById("c1").innerHTML='Item added to Cart!'
+}
+function cart2()
+{
+    document.getElementById("c2").innerHTML='Item added to Cart!'
+}
+function cart3()
+{
+    document.getElementById("c3").innerHTML='Item added to Cart!'
+}function cart4()
+{
+    document.getElementById("c4").innerHTML='Item added to Cart!'
+}function cart5()
+{
+    document.getElementById("c5").innerHTML='Item added to Cart!'
+}function cart6()
+{
+    document.getElementById("c6").innerHTML='Item added to Cart!'
+}
+function cart7()
+{
+    document.getElementById("c7").innerHTML='Item added to Cart!'
+}
+function cart8()
+{
+    document.getElementById("c8").innerHTML='Item added to Cart!'
+}
